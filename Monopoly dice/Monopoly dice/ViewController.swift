152779f55db325e099d9df0f7a18e132aae6be8d
Jakub Jirák <jakub@jakubjirak.com>
2019-12-01 00:52:48 +0100

    Use array instead of switch.

--- Monopoly dice/Monopoly dice/ViewController.swift
+++ Monopoly dice/Monopoly dice/ViewController.swift
@@ -11,47 +11,21 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var leftDiceImageView: UIImageView!
     @IBOutlet weak var rightDiceImageView: UIImageView!
-    
+
+    var leftDiceNumber = 1
+    var rightDiceNumber = 1
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        leftDiceImageView.image =  #imageLiteral(resourceName: "dice-one")
-        rightDiceImageView.image =  #imageLiteral(resourceName: "dice-one")
     }
     
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         //print("Button got tapped")
-        let numberLeft = Int.random(in: 1 ..< 6)
-        let numberRight = Int.random(in: 1 ..< 6)
-        switch numberLeft {
-            case 1:
-                leftDiceImageView.image =  #imageLiteral(resourceName: "dice-one")
-            case 2:
-                leftDiceImageView.image =  #imageLiteral(resourceName: "dice-two")
-            case 3:
-                leftDiceImageView.image =  #imageLiteral(resourceName: "dice-three")
-            case 4:
-                leftDiceImageView.image =  #imageLiteral(resourceName: "dice-four")
-            case 5:
-                leftDiceImageView.image =  #imageLiteral(resourceName: "dice-five")
-            default:
-                leftDiceImageView.image =  #imageLiteral(resourceName: "dice-six")
-        }
-        
-        switch numberRight {
-            case 1:
-                rightDiceImageView.image =  #imageLiteral(resourceName: "dice-one")
-            case 2:
-                rightDiceImageView.image =  #imageLiteral(resourceName: "dice-two")
-            case 3:
-                rightDiceImageView.image =  #imageLiteral(resourceName: "dice-three")
-            case 4:
-                rightDiceImageView.image =  #imageLiteral(resourceName: "dice-four")
-            case 5:
-                rightDiceImageView.image =  #imageLiteral(resourceName: "dice-five")
-            default:
-                rightDiceImageView.image =  #imageLiteral(resourceName: "dice-six")
-        }
+        leftDiceImageView.image = [#imageLiteral(resourceName: "dice-one"), #imageLiteral(resourceName: "dice-two"), #imageLiteral(resourceName: "dice-three"),#imageLiteral(resourceName: "dice-four"),#imageLiteral(resourceName: "dice-five"),#imageLiteral(resourceName: "dice-six")][leftDiceNumber]
+        rightDiceImageView.image = [#imageLiteral(resourceName: "dice-one"), #imageLiteral(resourceName: "dice-two"), #imageLiteral(resourceName: "dice-three"),#imageLiteral(resourceName: "dice-four"),#imageLiteral(resourceName: "dice-five"),#imageLiteral(resourceName: "dice-six")][rightDiceNumber]
+        leftDiceNumber = Int.random(in: 1 ..< 6)
+        rightDiceNumber = Int.random(in: 1 ..< 6)
     }
 }
 
