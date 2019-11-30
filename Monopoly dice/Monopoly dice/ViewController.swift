8b87bdec1b4e1799c2a429d59bf3814cfbfc175a
Jakub Jirák <jakub@jakubjirak.com>
2019-12-01 00:30:14 +0100

    Dice assets for third application

---
+++ Monopoly dice/Monopoly dice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Monopoly dice
+//
+//  Created by Jakub Jirák on 30/11/2019.
+//  Copyright © 2019 Jakub Jirák. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
