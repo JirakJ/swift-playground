a88d3606d812393a0909b94ebbf0b66016ff7403
Jakub Jirák <jakub@jakubjirak.com>
2019-12-01 14:53:12 +0100

    Innitial commit for new app Xylophone

---
+++ Xylophone/Xylophone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Jakub Jirák on 01/12/2019.
+//  Copyright © 2019 Jakub Jirák. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
