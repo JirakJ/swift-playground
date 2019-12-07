1210e595f10d2444eec96f3f5d8d97da1b437767
Jakub Jirák <jakub@jakubjirak.com>
2019-12-07 17:53:06 +0100

    Add Egg Timer project

--- Egg Timer/Egg Timer/ViewController.swift
+++ Egg Timer/Egg Timer/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let softTime = 5
+    let mediumTime = 8
+    let hardTime = 12
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        //print(sender.currentTitle)
+        let hardness = sender.currentTitle
+        switch hardness {
+        case "Soft":
+            print(softTime)
+        case "Medium":
+            print(mediumTime)
+        case "Hard":
+            print(hardTime)
+        default:
+            print("Default option")
+        }
+    }
+    
 }
 
