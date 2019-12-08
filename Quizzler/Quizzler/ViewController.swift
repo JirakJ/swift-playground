bf0c8862de0902b5c9fc475bd42c941ba5313ded
Jakub Jirák <jakub@jakubjirak.com>
2019-12-08 14:14:55 +0100

    Add assets for new applicatio called Quizzler

---
+++ Quizzler/Quizzler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Jakub Jirák on 08/12/2019.
+//  Copyright © 2019 Jakub Jirák. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
