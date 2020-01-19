cd1541541f5da21b12e8ca5418f1150d17bf39c9
Jakub Jirák <jakub@jakubjirak.com>
2020-01-19 20:11:05 +0100

    Add movie structure

---
+++ Movie_Tracker/Movie Tracker/Movie.swift
@@ -0,0 +1,27 @@
+//
+// Created by Jakub Jirák on 19/01/2020.
+// Copyright (c) 2020 JakubJirak. All rights reserved.
+//
+
+import Foundation
+
+struct Movie: Identifiable {
+    var id = UUID()
+    var title = ""
+    var rating = 3.0
+    var seen = false
+
+    init(title: String, rating: Double, seen: Bool) {
+        self.id = UUID()
+        self.title = title
+        self.rating = rating
+        self.seen = seen
+    }
+
+    init() {
+        self.id = UUID()
+        self.title = ""
+        self.rating = 3.0
+        self.seen = false
+    }
+}
