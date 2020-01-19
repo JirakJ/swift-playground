467859abc3659c302ab3e5371b98e6fe67c2876d
Jakub Jirák <jakub@jakubjirak.com>
2020-01-19 19:32:49 +0100

    Update content view

--- Movie_Tracker/Movie Tracker/ContentView.swift
+++ Movie_Tracker/Movie Tracker/ContentView.swift
@@ -9,13 +9,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var title = ""
+    @State var rating = 3.0
+    @State var seen = false
+
     var body: some View {
-        Text("Hello, World!")
+        List{
+            Section {
+                SectionTitle(title: "Title")
+                TextField("Movie Title", text: $title)
+            }
+            Section {
+                SectionTitle(title: "Rating")
+                HStack {
+                    Spacer()
+                    Text(String(repeating: "★", count: Int(rating))).foregroundColor(.yellow).font(.largeTitle)
+                    Spacer()
+                }
+                Slider(value: $rating, in: 1...5, step: 1)
+            }
+            Section {
+                SectionTitle(title: "Seen")
+                Toggle(isOn: $seen) {
+                    if title == "" {
+                        Text("I have seen this movie")
+                    } else {
+                        Text("I have seen \(title)")
+                    }
+                }
+            }
+            Section {
+                Button(action: {}) {
+                    HStack {
+                        Spacer()
+                        Text("Save").font(.largeTitle)
+//                        Text("Save").font(.largeTitle).padding(.all, 10).background(Color.blue).foregroundColor(.white).cornerRadius(10)
+                        Spacer()
+                    }
+                }
+            }
+        }.listStyle(GroupedListStyle())
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct SectionTitle: View {
+    var title: String
+
+    var body: some View {
+        Text(title).font(.caption).foregroundColor(.gray)
     }
 }
