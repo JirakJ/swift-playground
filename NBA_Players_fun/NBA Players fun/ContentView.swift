94adf43d47994af90380af9057d6feabe522315d
Jakub Jirák <jakub@jakubjirak.com>
2020-01-19 13:47:31 +0100

    Add new project NBA Players - swift UI

---
+++ NBA_Players_fun/NBA Players fun/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NBA Players Fun
+//
+//  Created by Jakub Jirák on 19/01/2020.
+//  Copyright © 2020 JakubJirak. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
