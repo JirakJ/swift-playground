bf614ac33c2f13a6534d4eb768d652756656b0cb
Jakub Jirák <jakub@jakubjirak.com>
2020-01-19 21:18:30 +0100

    Add new project Drawing test in swift ui

---
+++ Drawing_test/Drawing test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Drawing test
+//
+//  Created by Jakub Jirák on 19/01/2020.
+//  Copyright © 2020 JakubJirak. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
